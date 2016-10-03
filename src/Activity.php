0e3908783fbaf2cd302dcf51e298c0850065acf8
Ayana Powell and Mark Lawson <ayana@>
2016-10-03 10:23:40 -0700

    create Activity class with CRUD functionality

--- src/Activity.php
+++ src/Activity.php
@@ -7,7 +7,7 @@ class Activity
     private $trip_id;
     private $id;
 
-    function __construct($name, $date, $description, $trip_id, $id = null)
+    function __construct($name, $date, $trip_id, $description = "", $id = null)
     {
         $this->name = $name;
         $this->date = $date;
@@ -15,6 +15,64 @@ class Activity
         $this->trip_id = $trip_id;
         $this->id = $id;
     }
+// Standard Functions
+    function save()
+    {
+        $GLOBALS['DB']->exec("INSERT INTO activities (name, date, description, trip_id) VALUES ('{$this->name}', '{$this->date}', '{$this->description}', {$this->trip_id});");
+        $this->id = $GLOBALS['DB']->lastInsertId();
+    }
+
+    function update($new_name, $new_date, $new_description)
+    {
+        $GLOBALS['DB']->exec("UPDATE activities SET name = '{$new_name}', date = '{$new_date}', description = '{$new_description}' WHERE id = {$this->getId()};");
+        $this->setName($new_name);
+        $this->setDate($new_date);
+        $this->setDescription($new_description);
+    }
+
+    function delete()
+    {
+        $GLOBALS['DB']->exec("DELETE FROM activities WHERE id = {$this->getId()};");
+    }
+
+
+// Static Functions
+    static function findById($search_id)
+    {
+        $returned_activities = Activity::getAll();
+        $activity = null;
+        foreach($returned_activities as $returned_activity) {
+            if ($returned_activity->getId() == $search_id) {
+                $activity = $returned_activity;
+                break;
+            }
+        }
+        return $activity;
+    }
+
+    static function getAll()
+    {
+        $returned_activities = $GLOBALS['DB']->query("SELECT * FROM activities;");
+        $activities = array();
+        foreach($returned_activities as $activity)
+        {
+            $name = $activity['name'];
+            $date = $activity['date'];
+            $description = $activity['description'];
+            $trip_id = $activity['trip_id'];
+            $id = $activity['id'];
+            $new_activity = new Activity($name, $date, $trip_id, $description, $id);
+            array_push($activities, $new_activity);
+        }
+        return $activities;
+    }
+
+    static function deleteAll()
+    {
+        $GLOBALS['DB']->exec("DELETE FROM activities;");
+    }
+
+
 // Setters and getters
     function getName()
     {
---
+++ tests/ActivityTest.php
@@ -0,0 +1,132 @@
+<?php
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once 'src/Activity.php';
+    require_once 'src/Trip.php';
+
+    $server = 'mysql:host=localhost;dbname=trip_planner_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class ActivityTest extends PHPUnit_Framework_TestCase
+    {
+        protected function tearDown()
+        {
+            Activity::deleteAll();
+            Trip::deleteAll();
+        }
+
+        function test_getId()
+        {
+            $name = "Mall";
+            $date = "10/02/2016";
+            $trip_id = 1;
+            $descrition = "Shopping spree";
+            $new_activity = new Activity($name, $date, $trip_id, $descrition);
+            $new_activity->save();
+
+            $output = $new_activity->getId();
+
+            $this->assertEquals(true, is_numeric($output));
+        }
+
+        function test_save()
+        {
+            $name = "Mall";
+            $date = "10/02/2016";
+            $trip_id = 1;
+            $descrition = "Shopping spree";
+            $new_activity = new Activity($name, $date, $trip_id, $descrition);
+            $new_activity->save();
+
+            $output = Activity::getAll();
+
+            $this->assertEquals([$new_activity], $output);
+        }
+
+        function test_getAll()
+        {
+            $name = "Mall";
+            $date = "10/02/2016";
+            $trip_id = 1;
+            $descrition = "Shopping spree";
+            $new_activity = new Activity($name, $date, $trip_id, $descrition);
+            $new_activity->save();
+
+            $output = Activity::getAll();
+
+            $this->assertEquals([$new_activity], $output);
+        }
+
+        function test_deleteAll()
+        {
+            $name = "Mall";
+            $date = "10/02/2016";
+            $trip_id = 1;
+            $descrition = "Shopping spree";
+            $new_activity = new Activity($name, $date, $trip_id, $descrition);
+            $new_activity->save();
+
+            Activity::deleteAll();
+            $output = Activity::getAll();
+
+            $this->assertEquals([], $output);
+        }
+
+        function test_delete()
+        {
+            $name = "Mall";
+            $date = "10/02/2016";
+            $trip_id = 1;
+            $descrition = "Shopping spree";
+            $new_activity = new Activity($name, $date, $trip_id, $descrition);
+            $new_activity->save();
+
+            $name2 = "Mall";
+            $date2 = "10/02/2016";
+            $trip_id2 = 1;
+            $descrition2 = "Shopping spree";
+            $new_activity2 = new Activity($name2, $date2, $trip_id2, $descrition2);
+            $new_activity2->save();
+
+            $new_activity->delete();
+            $output = Activity::getAll();
+
+            $this->assertEquals([$new_activity2], $output);
+        }
+
+        function test_findById()
+        {
+            $name = "Mall";
+            $date = "10/02/2016";
+            $trip_id = 1;
+            $descrition = "Shopping spree";
+            $new_activity = new Activity($name, $date, $trip_id, $descrition);
+            $new_activity->save();
+
+            $output = Activity::findById($new_activity->getId());
+
+            $this->assertEquals($new_activity, $output);
+        }
+
+        function test_update()
+        {
+            $name = "Mall";
+            $date = "10/02/2016";
+            $trip_id = 1;
+            $descrition = "Shopping spree";
+            $new_activity = new Activity($name, $date, $trip_id, $descrition);
+            $new_activity->save();
+            $new_name = "Pioneer Mall";
+
+            $new_activity->update($new_name, $date, $descrition);
+
+            $this->assertEquals('Pioneer Mall', $new_activity->getName());
+        }
+    }
+
+?>
