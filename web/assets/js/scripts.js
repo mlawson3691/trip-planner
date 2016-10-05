5d71577a3692b3e880c6d0ccb269777e4b2c8ff4
Ayana Powell <ayanapowell1@gmail.com>
2016-10-05 08:04:34 -0700

    styles

--- web/assets/js/scripts.js
+++ web/assets/js/scripts.js
@@ -12,4 +12,59 @@ $(function() {
     $('.fa-times').click(function() {
         $('.review_form').slideUp();
     });
+
+/* ------- Introduction page -------- */   
+
+  $(".background_image").fadeIn(3000);
+
+  setTimeout(function(){
+    $("#pop-up").fadeIn("fast");
+    $("#pop-up").animate({
+      "width": "450px",
+      "height":"200px",
+  },700);
+  },900);
+
+
+  setTimeout(function(){
+   $(".intro").fadeIn("slow"); 
+ },1800);
+  
+  setTimeout(function(){
+    console.log(20);
+    $(".title").fadeOut("200");
+    $( ".subtitle" ).slideUp("slow");
+    },3600)
+
+  setTimeout(function(){
+    $("#pop-up").animate({
+        "height":"0px",
+        "border-style":"none", 
+        "transition":"0.5s ease"});
+    $(".background_image").fadeOut(900); 
+    },4200); 
+
+  setTimeout(function(){
+    $('#wrapper').addClass('hide');
+    $('.hidden-homepage').fadeIn('800');
+  },5100);
+
+  setTimeout(function(){
+    $(".intro_slides, .overlay").fadeIn(1500);
+  },6100);
+
+/* ------ slideshow ----- */ 
+  var index=0;
+  setTimeout(function mySlideshow(event){
+    var i;
+    var x=document.getElementsByClassName("my_slides");      
+    for (i=0 ; i < x.length; i++){
+      x[i].style.display="none";
+    }
+    index++;
+      if (index > x.length) {index=1}
+        x[index-1].style.display="block";
+      setTimeout(mySlideshow,5000)
+
+    },6100);    
 });
