bee1815c1837eade41400e0e8b4770dd44747f2a
Ayana Powell <ayanapowell1@gmail.com>
2016-10-06 15:06:18 -0700

    slideshow

--- web/assets/js/scripts.js
+++ web/assets/js/scripts.js
@@ -1,4 +1,8 @@
 $(function() {
+
+    $("#mycarousel").carousel({
+    interval: 3000}); 
+
     $('.show-form').click(function() {
         $(this).next().slideDown();
         $(this).addClass('hidden-form');
@@ -28,20 +32,7 @@ $(function() {
     $('.a-tag').click(function() {
         $(this).parent().next().next().next().toggle();
     });
-/* ------ slideshow ----- */
-  // var index=0;
-  // setTimeout(function mySlideshow(event){
-  //   var i;
-  //   var x=document.getElementsByClassName("my_slides");
-  //   for (i=0 ; i < x.length; i++){
-  //     x[i].style.display="none";
-  //   }
-  //   index++;
-  //     if (index > x.length) {index=1}
-  //       x[index-1].style.display="block";
-  //     setTimeout(mySlideshow,5000)
-  //
-  //   },6100);
+
 
     /* ------- user dashboard ------- */
     $('.user-edit-button').click(function(){
